7177bf9a934c8579aa8ad3f8493d9062077c63c3
kurnia22 <kurniawati.22.nk@gmail.com>
2017-05-30 16:43:04 +0700

    in progres import statements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import React from 'react';
+
+
+
 // create a new component. This component should produce 
 // some html
 
-const App = function(){
-    return <div> Hi! </div>;
+const App = function() {
+    return <div > Hi! < /div>;
 }
 
 // take this component's generated HTML and put it 
 // on the page (in the DOM)
 
+React.render(App);
