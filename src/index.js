6c8316eb5703d3bc191d1d61587792a72781b46a
kurnia22 <kurniawati.22.nk@gmail.com>
2017-05-30 16:32:45 +0700

    A taste of JSX

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// create a new component. This component should produce 
+// some html
+
+const App = function(){
+    return <div> Hi! </div>;
+}
+
+// take this component's generated HTML and put it 
+// on the page (in the DOM)
+
