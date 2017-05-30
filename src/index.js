1c0c24baf47753ce3c3e65a4615acb98e31e7ca6
kurnia22 <kurniawati.22.nk@gmail.com>
2017-05-30 17:32:04 +0700

    Render Targets

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 // create a new component. This component should produce 
 // some html
 
-const App = function() {
-    return <div > Hi! < /div>;
+const App = () => {
+    return <div > Hello World! < /div>;
 }
 
 // take this component's generated HTML and put it 
 // on the page (in the DOM)
 
-ReactDOM.render(<App/>);
+ReactDOM.render(<App/>, document.querySelector('.container'));
