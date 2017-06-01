3a5685290f347a981c388e9742c4e28956b15f0c
kurnia22 <kurniawati.22.nk@gmail.com>
2017-06-01 08:20:21 +0700

    Building List With Map

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
-const VideoLIst = (props) => {
-    return(
-        <ul className="col-md-4 list-group">
-            {props.videos.length}
-        </ul>
-    );
+const VideoListItem = (props) => {
+    return <li > Video < /li>
 };
 
-export default  VideoLIst;
+export default VideoListItem;
