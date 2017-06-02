21de7c39e995bf09f116e38e74013c2561cf0681
kurnia22 <kurniawati.22.nk@gmail.com>
2017-06-02 18:18:14 +0700

    handling null props

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const VideoDetail = ({video})=>{
+    if (!video){
+        return <div>Loading...</div>;
+    }
+
     const videoId = video.id.videoId;
-    const url = 'https://www.youtube.com/embed/${videoId}';
+    const url = `https://www.youtube.com/embed/${videoId}`;
 
     return(
         <div className="video-detail col-md-8">
@@ -16,3 +20,5 @@ const VideoDetail = ({video})=>{
         </div>
     );
 };
+
+export default VideoDetail;
